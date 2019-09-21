a948a4dcb31f991c12d7f4446e31206ffaa54ee8
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-21 13:15:42 -0300

    added minimal changes to use @xethya/bridge-phaser

--- src/Game.ts
+++ src/Game.ts
@@ -1,16 +1,23 @@
+import { XethyaPlugin } from "@xethya/bridge-phaser";
 import "phaser";
-import { Preloader } from './scenes/Preloader';
-import { Main } from './scenes/Main';
-    
-const config: GameConfig = {
-    type: Phaser.AUTO,
-    parent: "canvas",
-    width: 960,
-    height: 540,
-    scene: [
-        Preloader,
-        Main
-    ]
+import { Main } from "./scenes/Main";
+import { Preloader } from "./scenes/Preloader";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: "canvas",
+  width: 960,
+  height: 540,
+  scene: [Preloader, Main],
+  plugins: {
+    global: [
+      {
+        key: "XethyaPlugin",
+        plugin: XethyaPlugin,
+        start: true,
+      },
+    ],
+  },
 };
 
 const game = new Phaser.Game(config);
--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -1,18 +1,24 @@
 export class Main extends Phaser.Scene {
-    constructor() {
-        super("main");
-    }
+  constructor() {
+    super("main");
+  }
 
-    create() {
-        const logo = this.add.sprite(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'sheet1', 'phaser-logo');
-        
-        const music = this.sound.add('DOG');
-        this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-            if (!music.isPlaying) {
-                music.play();
-            }
+  create() {
+    const logo = this.add.sprite(this.sys.canvas.width / 2, this.sys.canvas.height / 2, "sheet1", "phaser-logo");
 
-            logo.setPosition(pointer.x, pointer.y);
-        });
-    }
+    const music = this.sound.add("DOG");
+    this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+      if (!music.isPlaying) {
+        music.play();
+      }
+
+      const d100 = this.add.d100();
+      const x = d100.die.roll();
+      const y = d100.die.roll();
+
+      console.log("I've offset your mouse position by X:", x, " and Y: ", y, " using a d100!");
+
+      logo.setPosition(pointer.x + x, pointer.y + y);
+    });
+  }
 }
